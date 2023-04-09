36ddf53ecc96bc359fe9a232d3bd4e8225dc0aee
jake.coyne <coyneer080backup@gmail.com>
2023-04-09 10:24:50 -0400

    tinyCMS config

---
+++ tina/config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "7d6c204c-e95d-4f18-9e66-4227b3a2ac8f", // Get this from tina.io
+  token: "3100bf7a363633ff9aa4ce0351dbbebcad26f351", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "./",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "./",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
