c10209eb75b9c0fff332a85992ba6d23e633aefe
jake.coyne <coyneer080backup@gmail.com>
2023-04-09 10:31:08 -0400

    update token

--- tina/config.ts
+++ tina/config.ts
@@ -6,7 +6,7 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 export default defineConfig({
   branch,
   clientId: "7d6c204c-e95d-4f18-9e66-4227b3a2ac8f", // Get this from tina.io
-  token: "3100bf7a363633ff9aa4ce0351dbbebcad26f351", // Get this from tina.io
+  token: "3fb36a9a1dcccdc22d78ecfc86eebf951e206f63", // Get this from tina.io
 
   build: {
     outputFolder: "admin",
